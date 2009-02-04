87a1e51c5d4c373fa44d1219b344046e8bf2feba
moaluko <moaluko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-04 18:01:10 +0000

    1.2.7 -store-locator.css CSS updates: *Commented out 'color' property for '#sl_div' ID to better match to different website CSS def'ns *Changed 'transparent' to 'white' for 'background' property of '#addressInput' ID
    git-svn-id: https://plugins.svn.wordpress.org/store-locator/tags/1.2.7@91914 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/store-locator-js.php
@@ -0,0 +1,31 @@
+<?php
+if (file_exists("./wp-config.php")){include("./wp-config.php");}
+elseif (file_exists("../wp-config.php")){include("../wp-config.php");}
+elseif (file_exists("../../wp-config.php")){include("../../wp-config.php");}
+elseif (file_exists("../../../wp-config.php")){include("../../../wp-config.php");}
+elseif (file_exists("../../../../wp-config.php")){include("../../../../wp-config.php");}
+elseif (file_exists("../../../../../wp-config.php")){include("../../../../../wp-config.php");}
+elseif (file_exists("../../../../../../wp-config.php")){include("../../../../../../wp-config.php");}
+elseif (file_exists("../../../../../../../wp-config.php")){include("../../../../../../../wp-config.php");}
+elseif (file_exists("../../../../../../../../wp-config.php")){include("../../../../../../../../wp-config.php");}
+include("../variables.sl.php");
+$zl=(trim(get_option('sl_zoom_level'))!="")? get_option('sl_zoom_level') : 4;
+print "if (document.getElementById('map')){window.onunload = GUnload;}
+		var add_base='".$sl_base."';		
+		var sl_map_home_icon='".get_option('sl_map_home_icon')."';
+		var sl_map_end_icon='".get_option('sl_map_end_icon')."';
+		var sl_google_map_country='".get_option('sl_google_map_country')."';
+		var sl_google_map_domain='".get_option('sl_google_map_domain')."';
+		var sl_zoom_level=$zl;";
+/*$home_size=getimagesize(get_option('sl_map_home_icon'));
+print "var sl_map_home_icon_width=$home_size[0];";
+print "var sl_map_home_icon_height=$home_size[1];";
+
+$end_size=getimagesize(get_option('sl_map_end_icon'));
+print "var sl_map_end_icon_width=$end_size[0];";
+print "var sl_map_end_icon_height=$end_size[1];";*/
+
+print "var sl_map_type=".get_option('sl_map_type').";";
+print "var sl_website_label='".get_option('sl_website_label')."';";
+
+?>
