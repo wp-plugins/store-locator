515ea8a34ef824ff6e18cede7d7db68db8c30701
moaluko <moaluko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-05 22:45:32 +0000

    1.2.27 -CSS: #sl_info_bubble update to avoid centering, line-height overspacing -store-locator-js.php update dynamically gets icon height, width now -store-locator.js accomdates the above now; added if statement for icon if named "sign" -functions.sl.php: line break btwn geocoding error msgs now -icons: added "_sign" to house, airport, food icon; added "blank.png"
    git-svn-id: https://plugins.svn.wordpress.org/store-locator/tags/1.2.27@123239 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/store-locator-js.php
@@ -0,0 +1,36 @@
+<?php
+if (file_exists("./wp-config.php")){include("./wp-config.php");}
+elseif (file_exists("../wp-config.php")){include("../wp-config.php");}
+elseif (file_exists("../../wp-config.php")){include("../../wp-config.php");}
+elseif (file_exists("../../../wp-config.php")){include("../../../wp-config.php");}
+elseif (file_exists("../../../../wp-config.php")){include("../../../../wp-config.php");}
+elseif (file_exists("../../../../../wp-config.php")){include("../../../../../wp-config.php");}
+elseif (file_exists("../../../../../../wp-config.php")){include("../../../../../../wp-config.php");}
+elseif (file_exists("../../../../../../../wp-config.php")){include("../../../../../../../wp-config.php");}
+elseif (file_exists("../../../../../../../../wp-config.php")){include("../../../../../../../../wp-config.php");}
+include("../variables.sl.php");
+$zl=(trim(get_option('sl_zoom_level'))!="")? get_option('sl_zoom_level') : 4;
+$mt=(trim(get_option('sl_map_type'))!="")? get_option('sl_map_type') : "G_NORMAL_MAP";
+$wl=(trim(get_option('sl_website_label'))!="")? get_option('sl_website_label') : "Website";
+$du=(trim(get_option('sl_distance_unit'))!="")? get_option('sl_distance_unit') : "miles";
+$oc=(trim(get_option('sl_map_overview_control'))!="")? get_option('sl_map_overview_control') : 0;
+print "if (document.getElementById('map')){window.onunload = GUnload;}
+var add_base='".$sl_base."';		
+var sl_map_home_icon='".get_option('sl_map_home_icon')."';
+var sl_map_end_icon='".get_option('sl_map_end_icon')."';
+var sl_google_map_country='".get_option('sl_google_map_country')."';
+var sl_google_map_domain='".get_option('sl_google_map_domain')."';
+var sl_zoom_level=$zl;
+var sl_map_type=$mt;
+var sl_website_label='$wl';
+var sl_load_locations_default='".get_option('sl_load_locations_default')."';
+var sl_distance_unit='$du';
+var sl_map_overview_control=$oc;";
+$home_size=getimagesize(ereg_replace($sl_base, $sl_path, get_option('sl_map_home_icon')));
+print "var sl_map_home_icon_width=$home_size[0];";
+print "var sl_map_home_icon_height=$home_size[1];";
+
+$end_size=getimagesize(ereg_replace($sl_base, $sl_path, get_option('sl_map_end_icon')));
+print "var sl_map_end_icon_width=$end_size[0];";
+print "var sl_map_end_icon_height=$end_size[1];";
+?>
