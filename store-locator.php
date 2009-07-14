40d979eb31b1077239d6d5f4d64f0b1f5f892763
moaluko <moaluko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-14 23:17:59 +0000

    1.2.28.2 - Incorporated fix for "Warning: Cannot modify header information – headers already sent ..." error occuring for those that have updated to WordPress Version 2.8.1
    git-svn-id: https://plugins.svn.wordpress.org/store-locator/tags/1.2.28.2@135166 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ store-locator.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Plugin Name: Store Locator
+Plugin URI: http://www.viadat.com/store-locator
+Description: A store locator / location finder plugin focused on providing mapping tools for web designers & developers who create sites in Wordpress & web site owners needing to show important stores or any other type of location.
+Version: 1.2.28.2
+Author: Viadat Creations
+Author URI: http://www.viadat.com
+*/
+
+$sl_version="1.2.28.2";
+$sl_db_version=1.2;
+include_once("variables.sl.php");
+include_once("functions.sl.php");
+include_once("via-latest.php");
+
+register_activation_hook( __FILE__, 'install_table');
+
+//add_action('wp_head', 'install_table');
+
+add_action('wp_head', 'head_scripts');
+// add_action('wp_footer', 'foot_scripts');
+
+add_filter('the_content', 'ajax_map', 7);
+
+add_action('admin_menu', 'sl_add_options_page');
+add_action('admin_print_scripts', 'add_admin_javascript');
+add_action('admin_print_styles','add_admin_stylesheet');
+
+load_plugin_textdomain($text_domain, "/wp-content/plugins/$sl_dir/languages/");
+
+add_filter('option_update_plugins', 'plugin_prevent_upgrade');
+add_filter('transient_update_plugins', 'plugin_prevent_upgrade');
+function plugin_prevent_upgrade($opt) {
+$plugin = plugin_basename(__FILE__);
+if ( $opt && isset($opt->response[$plugin]) ) {
+//Theres an update. Remove automatic upgrade:
+$opt->response[$plugin]->package = '';
+//Now we've prevented the upgrade taking place, It might be worth to give users a note that theres an update available:
+add_action("after_plugin_row_$plugin", 'plugin_update_disabled_notice');
+}
+return $opt;
+}
+function plugin_update_disabled_notice() {
+global $sl_dir;
+echo '<tr><td class="plugin-update" colspan="5" style="text-align:center">Use the <a href="./admin.php?page='.$sl_dir.'/news-upgrades.php"><b>Quick Updater</b></a> or <a href="http://www.viadat.com/vdl/store-locator.zip">Download the latest version</a></td></tr>';
+}
+?>
