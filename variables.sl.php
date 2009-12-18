657130e087c5b8817c03578ade2f986c9d6ca05d
moaluko <moaluko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-18 15:11:01 +0000

    1.2.36 - Added option to select character encoding set on 'Localization & Google API Key' page. Should help with supporting characters in many different languages
    git-svn-id: https://plugins.svn.wordpress.org/store-locator/tags/1.2.36@184397 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ variables.sl.php
@@ -0,0 +1,13 @@
+<?php
+
+global $sl_dir, $sl_base;
+$text_domain="lol";
+$sl_dir=dirname(plugin_basename(__FILE__)); //plugin absolute server directory name
+$sl_base=get_option('siteurl')."/wp-content/plugins/".$sl_dir; //URL to plugin directory
+//$sl_base=str_replace("http://$_SERVER[HTTP_HOST]", "", $sl_base);
+$sl_path=ABSPATH."wp-content/plugins/".$sl_dir; //absolute server pather to plugin directory
+//$prev=$wpdb->get_results("select ID, guid from ".$wpdb->prefix."posts where post_content like '%[STORE-LOCATOR%' AND post_type<>'revision' LIMIT 1", ARRAY_A);
+$view_link="| <a href='".get_option('siteurl')."/wp-admin/admin.php?page=$sl_dir/view-locations.php'>".__("View All Locations", $text_domain)."</a>";// | <a href='{$prev[0][guid]}' target='_blank'>".__("Preview User Interface", $text_domain)."</a>";
+$web_domain=$_SERVER[HTTP_HOST];
+$map_character_encoding=(get_option('sl_map_character_encoding')!="")? "&oe=".get_option('sl_map_character_encoding') : "";
+?>
