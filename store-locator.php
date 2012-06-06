64c031883fcc0f7f8347a917faf293fe77679bb3
viadat <viadat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-06 16:46:44 +0000

    1.8 - Added Hindi translation
    git-svn-id: https://plugins.svn.wordpress.org/store-locator/trunk@553943 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- store-locator.php
+++ store-locator.php
@@ -3,13 +3,13 @@
 Plugin Name: Store Locator
 Plugin URI: http://www.viadat.com/store-locator
 Description: A store locator plugin that gives you the ability to effectively show important locations in an easily searchable manner using Google Maps.
-Version: 1.7
+Version: 1.8
 Author: Viadat Creations
 Author URI: http://www.viadat.com
 */
 
-$sl_version="1.7";
-$sl_db_version=1.7;
+$sl_version="1.8";
+$sl_db_version=1.8;
 include_once("variables.sl.php");
 include_once("copyfolder.lib.php");
 include_once("functions.sl.php");
