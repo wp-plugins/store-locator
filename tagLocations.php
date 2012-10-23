2b3cc76985f74c1d7012fe83092fdebeefd439f7
viadat <viadat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-23 13:18:39 +0000

    1.9.3
    git-svn-id: https://plugins.svn.wordpress.org/store-locator/tags/1.9.3@616202 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tagLocations.php
@@ -0,0 +1,31 @@
+<?php
+//adding or removing tags for specified a locations
+if (!empty($_POST)) {extract($_POST);}
+
+//var_dump($sl_id); exit;
+if (is_array($sl_id)==1) {
+	$id_string=implode(",", $sl_id);
+} else {
+	$id_string=$sl_id;
+}
+if ($act=="add_tag") {
+	//adding tags
+	//print "UPDATE ".$wpdb->prefix."store_locator SET sl_tags=CONCAT(sl_tags, '".strtolower($sl_tags).", ') WHERE sl_id IN ($id_string)"; exit;
+	$wpdb->query("UPDATE ".$wpdb->prefix."store_locator SET sl_tags=CONCAT(sl_tags, ' ".prepare_tag_string(strtolower($sl_tags))." ') WHERE sl_id IN ($id_string)");
+	sl_process_tags($sl_tags, "insert", $sl_id);
+}
+elseif ($act=="remove_tag") {
+	//removing tags
+	if (empty($sl_tags)) {
+		//if no tag is specified, all tags will be removed from selected locations
+		$wpdb->query("UPDATE ".$wpdb->prefix."store_locator SET sl_tags='' WHERE sl_id IN ($id_string)");
+		sl_process_tags("", "delete", $id_string);
+	}
+	else {
+		//$wpdb->query("UPDATE ".$wpdb->prefix."store_locator SET sl_tags='".prepare_tag_string($sl_tags)."' WHERE sl_id IN ($id_string)");
+		$wpdb->query("UPDATE ".$wpdb->prefix."store_locator SET sl_tags=REPLACE(sl_tags, '$sl_tags,', '') WHERE sl_id IN ($id_string)");
+		$wpdb->query("UPDATE ".$wpdb->prefix."store_locator SET sl_tags=REPLACE(sl_tags, '{$sl_tags}&#44;', '') WHERE sl_id IN ($id_string)");
+		sl_process_tags("$sl_tags", "delete", $id_string);
+	}
+}
+?>
